d6b02376ddecfb691809bc2a2cb78e79a33e4943
DmitrijGribovskij <dmitrijgribovskij@gmail.com>
2016-11-29 20:50:50 +0300

    Turn

---
+++ chapter_001/src/main/java/arrayturn/Turn.java
@@ -0,0 +1,22 @@
+package arrayturn;
+/**
+ * Class Turn.
+ * @author Dmitrij Gribovskij
+ * @ since 29.11.2016
+ */
+ public class Turn {
+	 /**
+	 *This method overturns the array.
+	 *@param array - array
+	 *@return - array
+	 */
+	 public int[] back(int[] array) {
+		 int value;
+		 for (int i = 0; i < array.length / 2; i++) {
+			value = array[i];
+			array[i] = array[(array.length - 1) - i];
+			array[(array.length - 1) - i] = value;
+		 }
+		 return array;
+	 }
+}
---
+++ chapter_001/src/test/java/arrayturn/TurnTest.java
@@ -0,0 +1,24 @@
+package arrayturn;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Class TurnTest.
+ * @author Dmitrij Gribovskij
+ * @ since 29.11.2016
+ */
+ public class TurnTest {
+	 /**
+	 * ArrayTurnTest.
+	 */
+	 private Turn turn = new Turn();
+	 /**
+	 *Test method overturns the array.
+	 */
+	  @Test
+	 public void whenArrayThenTurnArray() {
+			final int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
+			final int[] expectArray = new int[]{7, 6, 5, 4, 3, 2, 1};
+			assertThat(turn.back(array), is(expectArray));
+	}
+}
